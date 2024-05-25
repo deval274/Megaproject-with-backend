d3ea0b55cd81f82e8f39e7f8172c5a24336eec58
Deval Ujeniya <devalujeniya@gmail.com>
2024-05-25 17:13:55 +0530

    add auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -127,13 +127,13 @@ const loginUser = asyncHandler(async (req, res) =>{
         throw new ApiError(404, "User does not exist")
     }
 
-   const isPasswordValid = await user.isPasswordCorrect(password)
+    const isPasswordValid = await user.isPasswordCorrect(password)
 
-   if (!isPasswordValid) {
+    if (!isPasswordValid) {
     throw new ApiError(401, "Invalid user credentials")
     }
 
-   const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
+    const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
 
     const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
